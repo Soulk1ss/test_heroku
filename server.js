17eba4e88623cc66f6cf921a16b76ad19ad899a5
mhusrefbee <mhusrefbee.a@careervisadigital.com>
2021-10-20 21:26:48 +0700

    initail project 2

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const cors = require('cors')
-
 app.use(cors())
+const server = require('http').createServer(app);
 
 app.get("/api", (req, res)=> {
     res.send("pass")
 })
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`server is run at port ${PORT}`);
 });
